efcebccd1645ef807fe8eb3e006e9d8fb2081c29
Ilya <IDolgopolov@bell-main.bellintegrator.ru>
2016-07-20 09:31:14 +0300

    added constraints, max speed

--- iSimpleSpeed/ViewController.swift
+++ iSimpleSpeed/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 import CoreLocation
+import Foundation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
     var locationMamager = CLLocationManager()
     var speed = 0.0
+    var maxSpeed = 0.0
     
 
     override func viewDidLoad() {
@@ -26,16 +28,37 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationMamager.startUpdatingLocation()
     }
     
+    func getMaxSpeed (one: Double, two: Double) -> Double {
+        if one > two {
+            return one
+        } else {
+            return two
+        }
+        
+    }
+    
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locationMamager.location {
-            speed = location.speed
-            speedLabel.text = "\(speed * 3.6)"
-            print ("\(speed * 3.6)")
+        if let location = locationMamager.location {	
+            speed = location.speed * 3.6
+            speedLabel.text = "\(Int(speed) ?? 0)"
+            maxSpeed = getMaxSpeed(maxSpeed,two: (speed * 3.6))
+            maxSpeedLabel.text = "\(Int(maxSpeed) ?? 0)"
+            print ("\(speed * 3.6) and max speed \(maxSpeed)")
         }
     }
     
 
     @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var maxSpeedLabel: UILabel!
 
 }
+
+let numberFormatter: NSNumberFormatter = {
+    let formatter = NSNumberFormatter()
+    formatter.locale = NSLocale.currentLocale()
+    formatter.numberStyle = .DecimalStyle
+    formatter.maximumFractionDigits = 1
+    formatter.groupingSeparator = " "
+    return formatter
+}()
 
